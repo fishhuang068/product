1d07a9716094b8be6c7fd38e063dd278777fb478
Fish <fishhuang068@gmail.com>
2021-04-25 23:37:07 +0800

    first commit

---
+++ products.py
@@ -0,0 +1,18 @@
+# 建立記帳程式專案 (二維清單)
+
+products = []
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格: ')
+    #p = []
+    #p.append(name)
+    #p.append(price)
+    
+    products.append([name, price])
+print(products)
+            
+print(products[0])
+print(products[0][0])
+print(products[0][1])
