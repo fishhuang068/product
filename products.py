274cd06000082020d133de158b02b4476fd18f98
Fish <fishhuang068@gmail.com>
2021-04-26 02:01:00 +0800

    refactor

--- products.py
+++ products.py
@@ -1,47 +1,64 @@
 # 建立記帳程式專案 (二維清單)
 
 import os #operation system 作業系統
-products = []
-if os.path.isfile('products.csv'):
-    print('yeah! 找到檔案了')
-    #讀取檔案
-    with open('products.csv', 'r', encoding = 'big5') as f:
+
+# 讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding = 'utf-8') as f:
         for line in f:
             if '商品,價格' in line:
                 continue
-            #s = line.strip().split(',')
-            #print(s)
             name, price = line.strip().split(',')
             products.append([name, price])
-    print(products)
-else:
-    print('找不到檔案....')
+    return products
         
-#讓使用者輸入        
-products = []
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格: ')
-    price = int(price)
-    #p = []
-    #p.append(name)
-    #p.append(price)
-    
-    products.append([name, price])
-print(products)
-  
-# print(products[0])
-# print(products[0][0])
-# print(products[0][1])
+# 讓使用者輸入   
+def user_input(products):   
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格: ')
+        price = int(price)
+        #p = []
+        #p.append(name)
+        #p.append(price)
+        # print(products[0])
+        # print(products[0][0])
+        # print(products[0][1])
+        products.append([name, price])
+    print(products)
+    return(products)
 
 # 印出所有購買紀錄
-for p in products:
-    print(p[0], '的價格', p[1])
- 
-# 寫入檔案 
-with open('products.csv', 'w', encoding = 'utf-8') as f:
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + str(p[1]) + '\n')
-        
+        print(p[0], '的價格', p[1])
+    
+# 寫入檔案 
+def write_file(filename, products):
+    with open(filename, 'w') as f:
+        f.write('商品,價格,\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+
+
+
+  
+
+# refactor 程式重構
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('yeah! 找到檔案了')
+        products = read_file(filename)
+    else:
+        print('找不到檔案....')    
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
